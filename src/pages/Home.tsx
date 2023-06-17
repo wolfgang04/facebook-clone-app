1a05f2f026abfdf48eab2d0040b88ed774cccf44
Andrew <carlandrewlinao@gmail.com>
2023-06-17 20:11:52 +0800

    add newsfeed component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Home/header/Header";
+import NewsFeed from "../components/Home/NewsFeed";
+
+const Home = () => {
+	return (
+		<div>
+			<Header />
+			<NewsFeed />
+		</div>
+	);
+};
+
+export default Home;
